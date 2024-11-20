399b56884821c4c663f4a07982b1775b2182d068
KZL <lz3450@outlook.com>
2024-11-20 16:03:59 -0600

    chore: update User-Agent string to reflect latest browser version

--- overleaf_sync.py
+++ overleaf_sync.py
@@ -240,7 +240,7 @@ class OverleafBroker:
             {
                 "Accept-Encoding": "gzip, deflate, br, zstd",
                 "Accept-Language": "en-US,en;q=0.9,zh;q=0.8",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
             }
         )
         self.username: str | None = None
