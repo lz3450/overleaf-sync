789afec491063da30b566b7ee8061e2cf8e6ad2f
KZL <lz3450@outlook.com>
2025-07-14 23:42:01 -0500

    scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -d "$HOME/.local/bin" ]]; then
-    mkdir -p "$HOME/.local/bin"
+    mkdir -vp "$HOME/.local/bin"
 fi
 
-ln -rsf overleaf_sync/overleaf_sync.py "$HOME/.local/bin/overleaf-sync"
+ln -vrsf overleaf_sync/overleaf_sync.py "$HOME/.local/lib/python3.13/site-packages/overleaf_sync"
