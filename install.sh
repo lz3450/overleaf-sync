bf857208328a5b17aa4acdf71940320e60bcabaa
KZL <lz3450@outlook.com>
2026-04-15 09:09:01 -0500

    update: install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ if [[ ! -d "$HOME/.local/bin" ]]; then
     mkdir -vp "$HOME/.local/bin"
 fi
 
-ln -vrsf overleaf_sync/overleaf_sync.py "$HOME/.local/lib/python3.13/site-packages/overleaf_sync"
+ln -vrsf overleaf_sync/overleaf_sync.py "$HOME/.local/lib/python3.14t/site-packages/overleaf_sync"
